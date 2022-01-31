b26d3f460e0ad82c94c4e6328fbd1015de7a00bf
Ramzy254 <fmwangi811@gmail.com>
2022-01-31 11:11:55 +0300

    Changes to styling Libraries

--- src/index.js
+++ src/index.js
@@ -1,26 +1,44 @@
 import React from "react";
 import ReactDom from "react-dom";
 
+// Import CSS
 import "./index.css";
+
+// Import Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// Book Objects
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL254_SR254,254_.jpg",
+    title: "The Real Anthony Fauci",
+    author: "Robert F. Kennedy Jr.",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL254_SR254,254_.jpg",
+    title: "The Real Anthony Fauci",
+    author: "Robert F. Kennedy Jr.",
+  },
+];
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
+      {books.map((book) => {
+        return <Book book={book} />;
+      })}
+      ;
     </section>
   );
 }
-
-function Book() {
+const Book = (props) => {
+  const { img, title, author } = props.book;
   return (
     <article className="book">
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71M4abh-afL._AC_UL254_SR254,254_.jpg"
-        alt=""
-      />
-      <h4>The Real Anthony Fauci</h4>
-      <h5>Robert F. Kennedy Jr.</h5>
+      <img src={img} alt={title} />
+      <h4>{title}</h4>
+      <h5>{author}</h5>
     </article>
   );
-}
+};
 
 ReactDom.render(<BookList />, document.getElementById("root"));
